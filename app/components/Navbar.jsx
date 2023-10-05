f9fb9396a1d226f08867ea9abf8a155a0390fea4
anukrati <qa.anukratirathor@gmail.com>
2023-10-05 09:01:09 +0530

    initial commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+export default function Navbar (){
+    <nav className="">
+    <Link className="text-white font-bold" href={"/"}>
+      GTCoding.
+    </Link>
+    {/* <Link href={"/addTopic"}>
+      Add Topic
+    </Link> */}
+  </nav>
+}
